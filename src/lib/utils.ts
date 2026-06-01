21462751ae8e1ffbe2cbdd28784c685ca3ad5da9
Daniil G <dealenx@gmail.com>
2026-06-01 07:13:48 +0700

    fix ci extra

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,12 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const basePath = process.env.NEXT_BASE_PATH ?? ""
+
+export function assetUrl(path: string): string {
+  if (path.startsWith("/")) {
+    return `${basePath}${path}`
+  }
+  return path
+}
